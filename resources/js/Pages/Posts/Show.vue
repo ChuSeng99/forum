ada4817aabec183b7cd7501b2afaba26292c9b75
Chu Seng <chuseng.ngu@geenet.com.sg>
2024-01-07 16:45:16 +0800

    first-commit

---
+++ resources/js/Pages/Posts/Show.vue
@@ -0,0 +1,85 @@
+<template>
+    <AppLayout :title="post.title">
+        <Container>
+            <h1 class="text-2xl font-bold">
+                {{ post.title }}
+            </h1>
+
+            <span class="block mt-1 text-sm text-gray-600">
+                {{ formattedDate }} ago by {{ post.user.name }}
+            </span>
+
+            <article class="mt-6">
+                <pre class="whitespace-pre-wrap font-sans">{{ post.body }}</pre>
+            </article>
+
+            <div class="mt-12">
+                <h2 class="text-xl font-semibold">Comments</h2>
+
+                <form v-if="$page.props.auth.user" @submit.prevent="addComment" class="mt-4">
+                    <div>
+                        <InputLabel
+                            for="body"
+                            class="sr-only"
+                        >Comment</InputLabel>
+                        <TextArea
+                            id="body"
+                            v-model="commentForm.body"
+                            rows="4"
+                            placeholder="Speak your mind Spock..."
+                        ></TextArea>
+                        <InputError :message="commentForm.errors.body" class="mt-1" />
+                    </div>
+
+                    <PrimaryButton
+                        type="submit"
+                        class="mt-3"
+                        :disabled="commentForm.processing"
+                    >Add Comment</PrimaryButton>
+                </form>
+
+                <ul class="divide-y mt-4">
+                    <li
+                        v-for="comment in comments.data"
+                        :key="comment.id"
+                        class="px-2 py-4"
+                    >
+                        <Comment :comment="comment" />
+                    </li>
+                </ul>
+
+                <Pagination
+                    :meta="comments.meta"
+                    :only="['comments']"
+                />
+            </div>
+        </Container>
+    </AppLayout>
+</template>
+
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+import { computed } from 'vue';
+import { relativeDate } from '@/Utilities/date';
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Container from '@/Components/Container.vue';
+import Comment from '@/Components/Comment.vue';
+import Pagination from '@/Components/Pagination.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextArea from '@/Components/TextArea.vue';
+import InputError from '@/Components/InputError.vue';
+
+const props = defineProps(['post', 'comments']);
+
+const formattedDate = computed(() => relativeDate(props.post.created_at));
+
+const commentForm = useForm({
+    body: '',
+});
+
+const addComment = () => commentForm.post(route('posts.comments.store', props.post.id), {
+    preserveScroll: true,
+    onSuccess: () => commentForm.reset(),
+});
+</script>
